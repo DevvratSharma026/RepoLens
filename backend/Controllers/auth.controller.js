0191a23e70e7df791b48dce40928f80c3fac5a0e
Devvrat Sharma <devsharma.pcm.2003@gmail.com>
2025-12-05 22:00:38 +0530

    implemented the login functionality

--- backend/Controllers/auth.controller.js
+++ backend/Controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const { default: bcrypt } = require("bcryptjs");
 const User = require("../models/User");
 const { issueOtpForUser, verifyOtpForUser } = require("../services/otp.service");
+const jwt = require('jsonwebtoken');
 
 exports.signup = async (req, res) => {
     try {
@@ -101,5 +102,72 @@ exports.verifyOtp = async (req, res) => {
     } catch (err) {
         const status = err.status || 500;
         return res.status(status).json({ success: false, message: err.message || "Verification failed" });
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User Not found"
+            });
+        }
+        if (!user.isVerified) {
+            return res.status(404).json({
+                success: false,
+                message: "Please verify your email via OTP first"
+            });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({
+                success: false,
+                message: 'Invalid credentials.',
+            });
+        }
+
+        const payload = {
+            email: user.email,
+            userId: user._id
+        }
+        const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '24h' });
+
+        const safeUser = user.toObject();
+        delete safeUser.password;
+        delete safeUser.__v;
+
+        const isProd = process.env.NDOE_ENV === 'production';
+        const cookieOptions = {
+            expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+            httpOnly: true,
+            secure: isProd,
+            sameSite: isProd ? 'none' : 'lax'
+        };
+
+        res
+            .cookie('token', token, cookieOptions)
+            .status(200).json({
+                success: true,
+                token,
+                user: safeUser,
+                message: "User login successfull",
+            });
+    } catch (err) {
+        onsole.error('Login error:', err);
+        return res.status(500).json({
+            success: false,
+            message: 'Cannot login right now.',
+        });
     }
 }
